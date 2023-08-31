635bb473f360e7109e5d61dfa3d892c25ee6c2f9
Shloketon <84617905+Shloketon@users.noreply.github.com>
2023-08-31 18:03:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings:["Problem Solver", "Data Scientist", "Data Analyst", "Frontend Developer", "SDE", "Data Storyteller", "Strategist"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop : true 
+});
